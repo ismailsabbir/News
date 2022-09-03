0d0b21c533da2d3002a5c5fa5e42390dbe85991e
ismailsabbir <ismail.diu.cse@gmail.com>
2022-09-03 22:11:34 +0600

    detals part

--- news.js
+++ news.js
@@ -1,3 +1,30 @@
+function categories() {
+  const url = `https://openapi.programming-hero.com/api/news/categories`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => categoriesdisplay(data.data.news_category));
+}
+function categoriesdisplay(categories) {
+  const newsContainer = document.getElementById("news-container");
+  for (const categori of categories) {
+    //console.log(categori);
+    const newele = document.createElement("div");
+    newele.classList.add("col");
+    newele.classList.add("col-3");
+    newele.classList.add("col-sm-3");
+    newele.classList.add("col-md-2");
+    newele.classList.add("col-lg-1");
+
+    newele.innerHTML = `
+    <p onclick="newsload('${categori.category_id}')">${categori.category_name}</p>
+    
+    `;
+
+    newsContainer.appendChild(newele);
+  }
+}
+categories();
+
 function bydefaultnews() {
   const url = `https://openapi.programming-hero.com/api/news/category/08`;
   fetch(url)
@@ -6,9 +33,10 @@ function bydefaultnews() {
 }
 function defaultnewsdisplay(defaults) {
   const newsContainer = document.getElementById("news-containerall");
+
   newsContainer.innerHTML = " ";
   for (const news of defaults) {
-    console.log(news);
+    //console.log(news);
     const newelement = document.createElement("div");
     newelement.classList.add("card");
     newelement.innerHTML = `
@@ -46,7 +74,9 @@ function defaultnewsdisplay(defaults) {
 
         </div>
         <div class="detals">
-          <i class="fa-solid fa-arrow-right"></i>
+        <button onclick="loaddetalinfo('${
+          news._id
+        }')" data-bs-toggle="modal" data-bs-target="#exampleModal" ><i class="fa-solid fa-arrow-right"></i></button>
         </div>
       </div>
 
@@ -55,34 +85,12 @@ function defaultnewsdisplay(defaults) {
   </div>
     `;
     newsContainer.appendChild(newelement);
+    document.getElementById("loder").style.display = "none";
   }
 }
 bydefaultnews();
+document.getElementById("loder").style.display = "block";
 
-function categories() {
-  const url = `https://openapi.programming-hero.com/api/news/categories`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => categoriesdisplay(data.data.news_category));
-}
-function categoriesdisplay(categories) {
-  const newsContainer = document.getElementById("news-container");
-  for (const categori of categories) {
-    console.log(categori);
-    const newele = document.createElement("div");
-    newele.classList.add("col");
-    newele.classList.add("col-3");
-    newele.classList.add("col-sm-3");
-    newele.classList.add("col-md-2");
-    newele.classList.add("col-lg-1");
-
-    newele.innerHTML = `
-    <p onclick="newsload('${categori.category_id}')">${categori.category_name}</p>
-    `;
-
-    newsContainer.appendChild(newele);
-  }
-}
 function newsload(id) {
   const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
   console.log(url);
@@ -94,7 +102,7 @@ function newsloaddetal(newss) {
   const newsContainer = document.getElementById("news-containerall");
   newsContainer.innerHTML = " ";
   for (const news of newss) {
-    console.log(news);
+    //console.log(news);
     const newelement = document.createElement("div");
     newelement.classList.add("card");
     newelement.innerHTML = `
@@ -132,7 +140,11 @@ function newsloaddetal(newss) {
 
         </div>
         <div class="detals">
-          <i class="fa-solid fa-arrow-right"></i>
+        <button onclick="loaddetalinfo('${
+          news._id
+        }')" data-bs-toggle="modal" data-bs-target="#exampleModal" ><i class="fa-solid fa-arrow-right"></i></button>
+          
+         
         </div>
       </div>
 
@@ -143,5 +155,21 @@ function newsloaddetal(newss) {
     newsContainer.appendChild(newelement);
   }
 }
-
-categories();
+function loaddetalinfo(id) {
+  const url = `https://openapi.programming-hero.com/api/news/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => modaldetals(data.data));
+}
+function modaldetals(detals) {
+  for (const detal of detals) {
+    console.log(detal);
+    document.getElementById("exampleModalLabel").innerText = `${detal.title}`;
+    const modalbody = document.getElementById("modal-body");
+    modalbody.innerHTML = `
+    <img class="img-fluid w-100" src="${detal.image_url}" alt="">
+    <p>${detal.details}</p>
+    <p>${detal.author.published_date}</p>
+  `;
+  }
+}
